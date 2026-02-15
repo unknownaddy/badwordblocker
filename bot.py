c04f28dc010e157f4a7c1a18290866fc531688d0
unknownaddy <unknownaddy2@gmail.com>
2026-02-15 15:30:34 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -7,7 +7,7 @@ import os
 intents = discord.Intents.default()
 intents.message_content = True 
 
-bot = commands.Bot(command_prefix='!', intents=intents)
+bot = commands.Bot(command_prefix=',', intents=intents)
 
 @bot.event
 async def on_ready():
